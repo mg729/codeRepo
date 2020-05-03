9477ee36e31a402f5f60050f87e7fe71a7a8d44c
mg729 <iammg729@gmail.com>
2020-05-03 17:29:21 +0900

    1920 update by using set and count function

--- BOJ_code/1920.cpp
+++ BOJ_code/1920.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdio>
-#include <algorithm>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -9,39 +9,29 @@ int main()
 {
 	int n;
 	scanf("%d", &n);
-	vector <int> vn;
+	set <int> s;
 	
 	for(int i = 0; i < n; ++i)
 	{
 		int val;
 		scanf("%d", &val);
-		vn.push_back(val);
+		s.insert(val);
 	}
 	
 	int m;
 	scanf("%d", &m);
-	vector <int> vm;
+	vector <int> v;
 	
 	for(int i = 0; i < m; ++i)
 	{
 		int val;
 		scanf("%d", &val);
-		vm.push_back(val);
+		v.push_back(val);
 	}
 	
-	vector <int> answer;
+	for(auto x : v)
+		printf("%d\n", s.count(x));
 
-	for(int j = 0; j < vm.size(); ++j)
-	{
-		if(find(vn.begin(), vn.end(), vm[j]) != vn.end())
-			answer.push_back(1);
-		else
-			answer.push_back(0);
-	}
-	
-	for(auto x : answer)
-		printf("%d\n", x);
-	
 	
 	return 0;
 }
