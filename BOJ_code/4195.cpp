a29b445d68cb6e640bceef04c20b3a0ef508c2a9
mg729 <iammg729@gmail.com>
2020-05-05 21:50:31 +0900

    4195 UnionFind concept

--- BOJ_code/4195.cpp
+++ BOJ_code/4195.cpp
@@ -1,49 +1,63 @@
 #include <iostream>
-#include <set>
-
+#include <cstdio>
+#include <map>
 /**
-Below solution has error problem
-
-4
-Ann Ben 2
-Carl David 2
-Evan Fred 2
-Betty Fred 7 --> not 7 but should be 3 
+Union-Find 알고리즘
+- 원소들의 연결여부를 확인하는 알고리즘
 */
+int parent[200001];
+int number[200001];
+char one[21];
+char two[21];
+
 using namespace std;
 
+int findParent(int p)
+{
+	if(p == parent[p])
+		return p;
+	return parent[p] = findParent(parent[p]);
+}
+
+void unionParent(int x, int y)
+{
+	x = findParent(x);
+	y = findParent(y);
+	
+	if(x!=y)
+	{
+		parent[y] = x;
+		number[x] += number[y];
+	}
+} 
 int main()
 {
 	int tc;
 	scanf("%d", &tc);
 	for(int i = 0; i < tc; ++i)
 	{
-		int numberVf;
-		scanf("%d", &numberVf);
+		map <string, int> data;
+		int cnt = 1;
+		int numberOfVf;
+		scanf("%d", &numberOfVf);
 		
-		set <string> vfs;
-		for(int i = 0; i <numberVf; ++i)
+		for(int i = 0 ; i < 2*numberOfVf; ++i)
 		{
-			char a[21] = {0,};
-			char b[21] = {0,};
-			scanf("%s %s", &a, &b);
+			parent[i] = i;
+			number[i] = 1;
+		}
+		for(int i = 0; i < numberOfVf; ++i)
+		{
+			scanf("%s %s", &one, &two);
 			
-			if(!vfs.count(a) && !vfs.count(b))
-			{
-				vfs.insert(a);
-				vfs.insert(b);
-				printf("%d\n", 2);
-			}
-			else
-			{
-				vfs.insert(a);
-				vfs.insert(b);
-				printf("%d\n", vfs.size());
-			}
-			for(auto x : vfs)
-				cout << x << " ";
-			cout << endl;
-		}	
+			if(data.count(one) == 0)
+				data[one] = cnt++;
+			if(data.count(two) == 0)
+				data[two] = cnt++;
+			
+			unionParent(data[one], data[two]);
+			printf("%d\n", number[findParent(data[one])]);
+		}
 	}
 	return 0;
 }
