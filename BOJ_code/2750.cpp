248cecdd5a4d244242c0a19106d77d230b737bcf
mg729 <iammg729@gmail.com>
2020-05-06 22:04:22 +0900

    2750 code update

--- BOJ_code/2750.cpp
+++ BOJ_code/2750.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <set>
-//#include <algorithm>  --> for sort algorithm 
 using namespace std;
 
 int main()
@@ -15,13 +14,6 @@ int main()
 		
 		s.insert(num);
 	}
-	
-//	sort(s.begin(),s.end()); --> ERROR 
-  	/**
-	set내부의 데이터는 이미 정렬되어 저장된다
-	오름차순이아닌 내림차순 정렬으로 출력을 원한다면
-	set의 멤버함수 rbegin 과 rend를 활용  
-	*/
 
 	for(auto x: s)
 		cout << x <<endl; 
