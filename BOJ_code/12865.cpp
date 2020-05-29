918d96a17f9af647318eb1ce83c366c81f430cb8
mg729 <iammg729@gmail.com>
2020-05-29 22:17:52 +0900

    [12865] dp vector-solution

--- BOJ_code/12865.cpp
+++ BOJ_code/12865.cpp
@@ -32,3 +32,47 @@ int main(void) {
 	
 	return 0;
 }
+
+/**
+// # Using C++ two dimension vector 
+   ## Be cautious with initialization!!
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+//solve By VECTOR
+vector< vector<int> > v (101,  vector<int>(100001,0));  --> if you don't intilize then, you can't insert data in vector like "v[i][j] = value;"
+
+int main(void) {
+	int n, k;
+	cin >> n >> k;
+	
+	for(int i = 1; i <= n; i++)
+	{
+		int weight;
+		int value;
+		cin >> weight >> value;
+		for(int j = 1; j <= k; j++)
+		{
+			if(i == 0 || j == 0)
+			{
+				v[i][j] = 0;
+			}
+			else if( j < weight )
+			{
+				v[i][j] = v[i-1][j];
+			}
+			else //j >= w
+			{
+				v[i][j] = max( v[i-1][j] , v[i-1][j-weight]+value);
+			}
+		}
+	}
+
+	cout << v[n][k];
+	
+	return 0;
+}
+
+*/
