46c0d1d3af0acd006ca860e98ca88d6476467a4f
mg729 <iammg729@gmail.com>
2020-08-11 21:08:06 +0900

    ch03_10808_O(n)_solution

--- Barking_code/10808.cpp
+++ Barking_code/10808.cpp
@@ -1,17 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int data[26];
+
 int main() {
-  string s = "abcdefghijklmnopqrstuvwxyz";
-  string str;
-  cin >> str;
-  for(int i = 0; i < s.size(); i++) {
-  	int count = 0;
-  	for(int j = 0; j <str.size(); j++) {
-	  if(s[i] == str[j]) {
-  	  	count++;
-	  }
+	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	string s;
+	cin >> s;
+	
+	for(auto c : s) 
+		data[c-'a']++;  // c-'a'를 해줌으로서 c가 'a'였다면 0, 'b'였다면 1, ···,  'z'였다면 25
+	for(int i = 0; i < 26; i++) {
+		cout << data[i] << ' ';
 	}
-	cout << count << " ";
-  }
 }
+//----- my solution
+//int main() {
+//  string s = "abcdefghijklmnopqrstuvwxyz";
+//  string str;
+//  cin >> str;
+//  for(int i = 0; i < s.size(); i++) {
+//  	int count = 0;
+//  	for(int j = 0; j <str.size(); j++) {
+//	  if(s[i] == str[j]) {
+//  	  	count++;
+//	  }
+//	}
+//	cout << count << " ";
+//  }
+//}
