5b534bbbd62988d9bd0d86f12287953b2d0633fb
mg729 <iammg729@gmail.com>
2020-08-28 22:28:08 +0900

    2504

--- Barking_code/0x08/2504.cpp
+++ Barking_code/0x08/2504.cpp
@@ -2,18 +2,49 @@
 
 using namespace std;
 
+stack<char> st;
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
+
+	string str;
+
+	int tmp = 1, sum = 0;
+	bool wrong = false;
+	cin >> str;
 	
-	string bracket;
-	cin >> bracket;
-	
-	int sum = 0;
-	
-	
-	
-	cout << sum;	
-	
+	for (int i = 0; str[i]; i++) {
+		if (str[i] == '(') {
+		  tmp *= 2;
+		  st.push('(');
+		} 
+		else if (str[i] == '[') {
+		  tmp *= 3;
+		  st.push('[');
+		} 
+		else if (str[i] == ')') {
+		  if (str[i - 1] == '(') sum += tmp;
+		  if (st.empty()) {
+		    wrong = true;
+		    break;
+		  }
+		  if (st.top() == '(') st.pop();
+		  tmp /= 2;
+		} 
+		else {
+		  if (str[i - 1] == '[') sum += tmp;
+		  if (st.empty()) {
+		    wrong = true;
+		    break;
+		  }
+		  if (st.top() == '[') st.pop();
+		  tmp /= 3;
+		}
+	}
+	if (wrong || !st.empty()) cout << 0;
+	else cout << sum;
+
+
 	return 0;
 }
