28f4d2a1d3938a3b302e07853f1d5ce6ecaa9742
mg729 <iammg729@gmail.com>
2020-08-30 23:51:32 +0900

    2480

---
+++ Barking_code/0x02/2480.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	for(int i = 0; i <3; i++) {
+		int val;
+		cin >> val;
+		v.push_back(val);
+	}
+	
+
+	
+	return 0;
+}
