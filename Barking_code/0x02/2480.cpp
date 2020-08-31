d894bad31b4e90cb06f3776a6429eb5ef0cd3525
mg729 <iammg729@gmail.com>
2020-08-31 21:17:51 +0900

    [ch02] 2480 basic quiz

--- Barking_code/0x02/2480.cpp
+++ Barking_code/0x02/2480.cpp
@@ -12,8 +12,26 @@ int main() {
 		cin >> val;
 		v.push_back(val);
 	}
-	
+	int prize = 0;
+	if(v[0] == v[1] &&
+	   v[1] == v[2] &&
+	   v[0] == v[2]) {
+	   	prize = 10000 + v[0] * 1000;
+	}
+	else if(v[0] == v[1]) {
+		prize = 1000 + v[0] * 100;	 	
+	}
+	else if(v[1] == v[2]) {
+		prize = 1000 + v[1] * 100;	 
+	} 
+	else if(v[0] == v[2]) {
+		prize = 1000 + v[2] * 100;	 
+	}
+	else {
+		sort(v.begin(), v.end());
+		prize = v[2] * 100;
+	}
 
-	
+	cout << prize;
 	return 0;
 }
