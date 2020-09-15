d2bcff706873f2a0d23a3dc655cc8b3c7f712015
mg729 <iammg729@gmail.com>
2020-09-15 18:57:38 +0900

    [ch09] 1926_solved

--- Barking_code/0x09/1926.cpp
+++ Barking_code/0x09/1926.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 #define x first
 #define y second
-int board[502][502]; // 1이 색칠된부분,  0이 색칠안 된 부분 
+int board[502][502];
 bool vis[502][502];
 int n;
 int m;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-queue<pair<int,int> > q;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -27,21 +26,24 @@ int main() {
 	int maxCount = 0;
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < m ; j++) {
-			if(!board[i][j]) continue;
-			if(board[i][j] && !vis[i][j]) {
-				q.push({i,j});
-				picNum++;
-			}
+			if(!board[i][j] || vis[i][j]) continue;
+			
+			queue<pair<int,int> > q;
+			q.push({i,j});
+			vis[i][j] = 1; //*****!!!! always initialize first vis point
+			picNum++;
+			
 			int picCount = 0;
 			while(!q.empty()) {
 				pair<int,int> cur = q.front();
 				q.pop();
+				picCount++; // picCount is incresed outside of below for loop
 				for(int dir = 0; dir <4; dir++) {
 					int nx = cur.x + dx[dir];
 					int ny = cur.y + dy[dir];
 					if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 					if(vis[nx][ny] || !board[nx][ny]) continue;
-					picCount++;
+					
 					vis[nx][ny] = 1;
 					q.push({nx,ny});
 				}
