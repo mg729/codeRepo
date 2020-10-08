c587c0d1b224efd514630a0cf8da9b9c19455f45
mg729 <iammg729@gmail.com>
2020-10-08 22:27:36 +0900

    [ch09] TWO BFS start point

--- Barking_code/0x09/4179.cpp
+++ Barking_code/0x09/4179.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 #define x first
 #define y second
+
 string board[1002];
 
-int dist[1002][1002];
+int fire[1002][1002]; //불에대한 전파속도 BFS  큐
+int move[1002][1002]; 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -17,38 +19,38 @@ int main() {
 	int r,c;
 	cin >> r >> c;
 	
-	queue <pair<int, int> > q;
+	queue <pair<int, int> > q_fire;
+	queue <pair<int, int> > q_move;
 	
 	for(int i = 0; i < r; i++) {
 		cin >> board[i];
 	}
 	for(int i = 0; i < r; i++) {
 		for(int j = 0; j < c; j++) {
-			if(board[i][j] == 'J') 
-				q.push({i,j});
+			if(board[i][j] == 'F')  // string board one dimension can be accessed two dimension char[][] type
+				q_fire.push({i,j});
+			else if(board[i][j] == 'J')
+				q_move.push({i,j});
 		}
 	}
-	int min = 0;
-	while(!q.empty()) {
-		pair<int, int> cur = q.front();
-		q.pop();
+	
+	while(!q_fire.empty()) {
+		pair<int, int> cur = q_fire.front();
+		q_fire.pop();
 		
 		for(int dir = 0; dir < 4; dir++) {
 			int nx = cur.x + dx[dir];
 			int ny = cur.y + dy[dir];
-			
-			if(board[nx][ny] == 'F') {
-				min = dist[cur.x][cur.y];
-				break;
-			}
+
 			if(nx < 0 || nx >= r || ny < 0 || ny >= c) continue;  // ** >= r , >= c  do not forget equal condition
-			if(board[nx][ny] == '#' || dist[nx][ny] ) continue;
+			if(board[nx][ny] == '#' || fire[nx][ny] ) continue;
 			
-			dist[nx][ny] = dist[cur.x][cur.y] + 1;
-			q.push({nx,ny});
+			fire[nx][ny] = fire[cur.x][cur.y] + 1;
+			q_fire.push({nx,ny});
 		}
 	}
-	if()
+	
+	
 	
 	return 0;
 }
