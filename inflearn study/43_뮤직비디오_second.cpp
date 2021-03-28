b691949ba6557b63a1d471e533fbc901ba5cde34
unknown <iammg729@gmail.com>
2021-03-28 22:50:36 +0900

    [quiz43] review

---
+++ inflearn study/43_뮤직비디오_second.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m, val;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	freopen("input.txt", "rt", stdin);
+	cin >> n >> m;
+
+	cout << res;
+	return 0;
+}
