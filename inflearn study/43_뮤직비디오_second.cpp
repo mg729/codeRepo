e7a5e4c5707c1158d18e224cceaf0e6e7a4e843d
unknown <iammg729@gmail.com>
2021-03-29 23:11:41 +0900

    [quiz44] binary search review

--- inflearn study/43_뮤직비디오_second.cpp
+++ inflearn study/43_뮤직비디오_second.cpp
@@ -1,16 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m, val;
+int n, m, val, lt, mid, rt, res;
+vector<int> v;
 
-
+int calculateSize(int size) {
+	int cnt = 1;
+	int sum = 0;
+	for(int i = 0; i <n; i++) {
+		if(sum + v[i] > size) {
+			cnt++;
+			sum = v[i];
+		}
+		else sum += v[i];
+	}
+	return cnt;
+}
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	freopen("input.txt", "rt", stdin);
+//	freopen("input.txt", "rt", stdin);
 	cin >> n >> m;
-
+	
+	res = -1;
+	for(int i = 0; i < n; i++) {
+		cin >> val;
+		v.push_back(val);
+		rt += val;
+		res = max(val, res);
+	}
+	
+	int minRes = res;
+	lt = 1;
+	while(lt < rt) {	
+		mid = (lt+rt) / 2; // mid value will be set as minimum dvd size
+		if(calculateSize(mid) <= m) {
+			res = mid;
+			rt = mid - 1;
+		}
+		else lt = mid + 1;
+	}
+	res = max(minRes, res);
 	cout << res;
 	return 0;
 }
