83163176ae44017bddc8757848683078552e8167
unknown <iammg729@gmail.com>
2021-04-14 22:45:28 +0900

    [quiz59] finding subset clean code

--- inflearn study/59_부분집합.cpp
+++ inflearn study/59_부분집합.cpp
@@ -4,23 +4,38 @@ using namespace std;
 int n;
 int check[12];
 
-void dfs(int level) {
-	if(level > n) return;
-	
-	check[level] = 1;
-	dfs(level + 1);
-	if(level==n) {
-		for(int i = 1; i <= n; i++) 
-			if(check[i]) cout << i << " ";
-		cout << "\n";
-	}
+//void dfs(int level) {
+//	if(level > n) return;
+//	
+//	check[level] = 1;
+//	dfs(level + 1);
+//	if(level==n) {
+//		for(int i = 1; i <= n; i++) 
+//			if(check[i]) cout << i << " ";
+//		cout << "\n";
+//	}
+//
+//	check[level] = 0;
+//	dfs(level + 1);
+//	if(level==n) {
+//		for(int i = 1; i <= n; i++) 
+//			if(check[i]) cout << i << " ";
+//		cout << "\n";
+//	}
+//}
 
-	check[level] = 0;
-	dfs(level + 1);
-	if(level==n) {
-		for(int i = 1; i <= n; i++) 
+void dfs(int level) {
+	if(level > n) {
+		for(int i = 1 ; i <= n ; i++) 
 			if(check[i]) cout << i << " ";
 		cout << "\n";
+		return;
+	}
+	else {
+		check[level] = 1;
+		dfs(level+1);
+		check[level] = 0;
+		dfs(level+1);
 	}
 }
 
@@ -28,7 +43,7 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-//	freopen("input.txt", "rt", stdin);
+	freopen("input.txt", "rt", stdin);
 	cin >> n;
 	dfs(1);
 
