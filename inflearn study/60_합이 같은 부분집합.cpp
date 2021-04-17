36cb877275e22bf303473833d807bfef60897800
unknown <iammg729@gmail.com>
2021-04-17 22:11:30 +0900

    [quiz60] subset sum

---
+++ inflearn study/60_합이 같은 부분집합.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, val;
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	freopen("input.txt", "rt", stdin);
+	
+	for(int i = 0; i < n; i++) {
+		cin >> val;
+		v.push_back(val);
+	}
+	
+	bool isEqualSum = true;
+	for(int i = 0; i < n; i++) {
+		
+	}
+	
+	if(isEqualSum) cout << "YES";
+	else cout << "NO";
+	
+	return 0;
+}
