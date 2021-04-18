7ba25abcc1f91f8e7636dedb718d4981856289aa
unknown <iammg729@gmail.com>
2021-04-18 23:47:01 +0900

    [quiz60] continue to solve

--- inflearn study/60_합이 같은 부분집합.cpp
+++ inflearn study/60_합이 같은 부분집합.cpp
@@ -2,22 +2,27 @@
 using namespace std;
 
 int n, val;
+int sumArr[58];
 vector<int> v;
+bool isEqualSum; // false
+
+void dfs(int x) {
+	
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	freopen("input.txt", "rt", stdin);
 	
+	cin >> n;
+	
 	for(int i = 0; i < n; i++) {
 		cin >> val;
 		v.push_back(val);
 	}
 	
-	bool isEqualSum = true;
-	for(int i = 0; i < n; i++) {
-		
-	}
+	dfs(n);
 	
 	if(isEqualSum) cout << "YES";
 	else cout << "NO";
