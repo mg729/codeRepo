5e9dcd91b51e855161b7c9f7c8a20803b209860b
unknown <iammg729@gmail.com>
2021-04-18 23:57:07 +0900

    [quiz60] subset ps

--- inflearn study/60_합이 같은 부분집합.cpp
+++ inflearn study/60_합이 같은 부분집합.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 int n, val;
 int sumArr[58];
+int check[12];  // n : 1 ~ 10
 vector<int> v;
 bool isEqualSum; // false
 
 void dfs(int x) {
-	
+	if(x > n){
+		int localSum = 0;
+		for(int i = 0; i < n; i++) {
+			if(check[i]) localSum += v[i];
+		}
+		if(sumArr[localSum]) {
+			isEqualSum = true;
+			return;
+		}
+		sumArr[localSum]++;
+		return;
+	}
+	check[x] = 1;
+	dfs(x+1);
+	check[x] = 0;
+	dfs(x+1);	
 }
 
 int main() {
@@ -22,7 +38,7 @@ int main() {
 		v.push_back(val);
 	}
 	
-	dfs(n);
+	dfs(1);
 	
 	if(isEqualSum) cout << "YES";
 	else cout << "NO";
