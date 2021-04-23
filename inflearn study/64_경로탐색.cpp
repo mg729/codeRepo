303a72ab23a1b1d20193d973ae6fad2cdb60e728
unknown <iammg729@gmail.com>
2021-04-23 22:55:56 +0900

    [quiz64] wrong answer

---
+++ inflearn study/64_경로탐색.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m, cnt;
+int matrix[22][22];
+
+void dfs(int level) {
+	if(level >= n) {
+		cnt++;
+		return;
+	}
+	
+	for(int i = 1; level+i <= n; i++) {
+		if(matrix[level][level+i])
+			dfs(level+1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+//	freopen("input.txt", "rt", stdin);
+	
+	cin >> n >> m;
+	for(int i = 1; i <= m; i++) {
+		int v1, v2; // vertex
+		cin >> v1 >> v2;
+		matrix[v1][v2] = 1;
+	}
+	
+	dfs(1);
+	cout << cnt;
+
+	return 0;
+}
