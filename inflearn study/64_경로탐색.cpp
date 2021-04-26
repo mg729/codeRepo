c3cca9b2d6a855ee5b3ee59aa4322fd042038a7b
unknown <iammg729@gmail.com>
2021-04-26 23:19:12 +0900

    [quiz64] dfs - check Array utilization

--- inflearn study/64_경로탐색.cpp
+++ inflearn study/64_경로탐색.cpp
@@ -3,26 +3,34 @@ using namespace std;
 
 int n, m, cnt;
 int matrix[22][22];
+int check[22];
+int pathArr[22];
 
-void dfs(int level) {
-	if(level >= n) {
+void dfs(int level, int path) {
+	if(level == n) {
+		for(int i = 0; i < path; i++) {
+			cout << pathArr[i] << " ";
+		}
+		cout << endl;
 		cnt++;
 		return;
 	}
 	
-	// How to search 4 -> 2 -> 5 route???? 
-	for(int i = 1; level + i <= n; i++) {
-		if(matrix[level][level+i]) {
-			cout << level << " ";
-			dfs(level+i);	
+	for(int i = 1; i <= n; i++) {
+		if(matrix[level][i] && check[i] == 0) {
+			check[i] = 1;
+			pathArr[path] = i;
+			dfs(i, path+1);
+			check[i] = 0;
 		}
 	}
+	
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-//	freopen("input.txt", "rt", stdin);
+	freopen("input.txt", "rt", stdin);
 	
 	cin >> n >> m;
 	for(int i = 1; i <= m; i++) {
@@ -30,8 +38,10 @@ int main() {
 		cin >> v1 >> v2;
 		matrix[v1][v2] = 1;
 	}
-	
-	dfs(1);
+
+	check[1] = 1;
+	pathArr[0] = 1;
+	dfs(1, 1);
 	cout << cnt;
 
 	return 0;
