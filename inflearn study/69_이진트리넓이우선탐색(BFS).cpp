a396b09798db7740b6a9cd7ba224ff8f3abeae03
unknown <iammg729@gmail.com>
2021-05-04 00:25:52 +0900

    [quiz69] BFS - queue library

--- inflearn study/69_이진트리넓이우선탐색(BFS).cpp
+++ inflearn study/69_이진트리넓이우선탐색(BFS).cpp
@@ -1,6 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int ch[10];
+queue<int> q;
+vector<int> v[10];
+int x;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	freopen("input.txt", "rt", stdin);
+	
+	for(int i = 1; i <=6; i++) {
+		int n1, n2; // node1, node2
+		cin>> n1 >> n2;
+		v[n1].push_back(n2);
+//		v[n2].push_back(n1);
+	}
+	
+	q.push(1);
+	ch[1] = 1;
+	while(!q.empty()) {
+		x = q.front();
+		cout << x << " ";
+		for(int i = 0; i < v[x].size(); i++) {
+			if(ch[v[x][i]] == 0) {
+				ch[v[x][i]] = 1;
+				q.push(v[x][i]);
+			}
+		}
+		q.pop();
+	}
+	return 0;
+}
+
+//Solution 2--------------
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
 int Queue[100];
 int front = -1;
 int back = -1;
@@ -17,7 +55,7 @@ int main() {
 		int n1, n2; // node1, node2
 		cin>> n1 >> n2;
 		v[n1].push_back(n2);
-		v[n2].push_back(n1);
+//		v[n2].push_back(n1);
 	}
 	
 	Queue[++back] = 1;
@@ -34,7 +72,9 @@ int main() {
 	}
 	return 0;
 }
+*/
 
+//Solution 3--------------
 /*
 #include<bits/stdc++.h>
 using namespace std;
