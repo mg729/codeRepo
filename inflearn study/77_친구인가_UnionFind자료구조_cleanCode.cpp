5689da44c9dcf3400dd0e4872ba9ebc4491b3e47
unknown <iammg729@gmail.com>
2021-06-18 16:31:25 +0900

    [quiz77] UnionFind

--- inflearn study/77_친구인가_UnionFind자료구조_cleanCode.cpp
+++ inflearn study/77_친구인가_UnionFind자료구조_cleanCode.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int unf[1002];
+int unf[1002]; // tree
 
 int Find(int v) {
 	if(v == unf[v]) return v;
-	else return unf[v] = Find(unf[v]); //경로 압축  
+	else return unf[v] = Find(unf[v]); //경로 압축 - memoization  
 }
 
 void Union (int a, int b){
-	a = Find(a);
-	b = Find(b);
-	if(a != b) unf[a] = b;
+    int node1 =  Find(a);
+	int node2 = Find(b);
+	if(node1 != node2) unf[node1] = node2; //organize tree structure
 }
 
 int main() {
